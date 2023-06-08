a32754cc0e6d6153bccf60efdb011a689aa2cf1e
marcelino230 <63226939+marcelino230@users.noreply.github.com>
2023-06-08 10:52:51 +0700

    new update soal1.js
    new update soal1.js

--- soal1.js
+++ soal1.js
@@ -1,13 +1,25 @@
+const readline = require('readline');
+
 function calculateSquareRoot() {
-  const x = prompt('Masukkan angka genap:');
-  if (x < 0) {
-    console.log('Tidak bisa input bilangan negatif');
-  } else if (x % 2 !== 0) {
-    console.log('Tidak bisa input bilangan ganjil');
-  } else {
-    const result = Math.sqrt(x);
-    console.log(`Akar pangkat 2 dari ${x} adalah ${result}`);
-  }
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('Masukkan angka genap: ', (x) => {
+    x = parseInt(x);
+
+    if (x < 0) {
+      console.log('Tidak bisa input bilangan negatif');
+    } else if (x % 2 !== 0) {
+      console.log('Tidak bisa input bilangan ganjil');
+    } else {
+      const result = Math.sqrt(x);
+      console.log(`Akar pangkat 2 dari ${x} adalah ${result}`);
+    }
+
+    rl.close();
+  });
 }
 
 calculateSquareRoot();
